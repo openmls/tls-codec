acdd1e10ca4a8aec247a702da1c0da80a71cd724
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2021-06-29 08:36:31 +0000

    deploy: d18d64872ae3c7501a664f58d17cdb5e74129995

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["tls_codec"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"tls_codec/enum.Error.html\" title=\"enum tls_codec::Error\">Error</a>","synthetic":false,"types":["tls_codec::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

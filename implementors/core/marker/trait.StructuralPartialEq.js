462a484f67db3de6081c785ccfb35a5eedd28ce5
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2021-10-14 07:53:05 +0000

    deploy: e4eaafff0c521699300c393ebf7e5512e386cde8

--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["tls_codec"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"tls_codec/enum.Error.html\" title=\"enum tls_codec::Error\">Error</a>","synthetic":false,"types":["tls_codec::Error"]}];
+implementors["tls_codec"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"tls_codec/enum.Error.html\" title=\"enum tls_codec::Error\">Error</a>","synthetic":false,"types":["tls_codec::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
